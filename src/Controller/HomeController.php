2e9dc409f5629d5ac517f5e3d99580de65a18c06
veronj <veron_j@etna-alternance.net>
2019-03-14 12:53:02 +0100

    form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
-use GuzzleHttp\Client;
+use App\Entity\Client;
+use App\Form\ClientType;
+use GuzzleHttp\Client as httpClient;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,12 +24,37 @@ class HomeController extends AbstractController
     }
 
     /**
+     * @Route("/client", name="client")
+     */
+    public function client(Request $request, ObjectManager $em)
+    {
+
+        $client = new Client();
+        $form = $this->createForm(ClientType::class, $client);
+        
+        $form->handleRequest($request);
+
+         if ($form->isSubmitted() && $form->isValid())
+        {
+
+
+            $this->em->persist($client);
+            $this->em->flush();
+            return $this->redirectToRoute('home.client.index');
+        }
+
+        return $this->render('home/index.html.twig', [
+            'client' => $client,
+            'form' => $form->createView()
+        ]); 
+    }
+
+    /**
      * @Route("/api", name="api")
      */
     public function access()
     {
-        $client = new Client();
-
+        $client = new httpClient();
         $res = $client->request('POST', 'https://homologation.lydia-app.com/api/request/do.json', [
         'message' => 'hello world',
         'amount'   => '12.20',
@@ -36,16 +65,15 @@ class HomeController extends AbstractController
         "user_token" => "+33600000001"
         ]);
 
-        
         return new Response($res->getBody());
     }
 
-        /**
+    /**
      * @Route("/git", name="git")
      */
     public function github()
     {
-        $client = new Client();
+        $client = new httpClient();
         $res = $client->request('GET', 'https://api.github.com/user');
         echo $res->getStatusCode();
         // "200"
@@ -53,5 +81,41 @@ class HomeController extends AbstractController
         // 'application/json; charset=utf8'
         echo $res->getBody();
     }
+
+    /**
+     * @Route("/sig", name="sig")
+     */
+    public function makeSig()
+    {
+        // Prepare data
+        $param = array(
+            'message' => 'hello world',
+            'amount'   => '12.20',
+            'currency' => 'EUR',
+            'type' => 'phone',
+            "recipient" => "+33600000001",
+            "vendor_token" => "xx",
+            "user_token" => "+33600000001"
+        );
+
+        ksort($param); // alphabetical sorting
+
+        $sig = array();
+
+        foreach ($param as $key => $val) {
+            $sig[] .= $key.'='.$val;
+        }
+        
+        
+        // Concat the private token (provider one or vendor one) and has the result
+        $callSig = md5(implode("&", $sig)."xx");
+
+        return $this->render('base.html.twig', [
+            'callSig' => $callSig,
+            'param' => $param
+        ]);
+
+    }
+    
 
 }
